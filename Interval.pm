494283eab1d7ecbd1d941e1328e715f52c0f31d6
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-06-15 02:52:57 +0000

    rename as Number::Interval, changing all references to the OMP and "range"
    svn path=/trunk/perlmods/; revision=7389

--- Interval.pm
+++ Interval.pm
@@ -1,28 +1,33 @@
-package OMP::Range;
+package Number::Interval;
 
 =head1 NAME
 
-OMP::Range - Implement simple ranges
+Number::Interval - Implement a representation of a numeric interval
 
 =head1 SYNOPSIS
 
-  use OMP::Range;
+  use Number::Interval;
 
-  $r = new OMP::Range( Min => -4, Max => 20);
-  $r = new OMP::Range( Min => 0 );
+  $i = new Number::Interval( Min => -4, Max => 20);
+  $i = new Number::Interval( Min => 0 );
 
-  print "$r";
+  $is = $i->contains( $value );
+  $status = $i->intersection( $i2 );
+
+  print "$i";
 
 =head1 DESCRIPTION
 
-Simple class to implement a closed or open range. Exists mainly
-to allow stringification override.
+Simple class to implement a closed or open interval. Can be used to
+compare different intervals, determine set membership, calculate
+intersections and provide default stringification methods.
 
-Ranges can be bound or unbound. If C<max> is less than C<min>
-the range is inverted.
+Intervals can be bound or unbound. If C<max> is less than C<min>
+the interval is inverted.
 
 =cut
 
+use 5.006;
 use strict;
 use warnings;
 use Carp;
@@ -30,7 +35,10 @@ use overload
   '""' => "stringify",
   'eq' => "equate";
 
-our $VERSION = (qw$Revision$ )[1];
+# CVS ID: $Id$
+
+use vars qw/ $VERSION /;
+$VERSION = '0.01';
 
 =head1 METHODS
 
@@ -43,8 +51,8 @@ our $VERSION = (qw$Revision$ )[1];
 Create a new object. Can be populated when supplied with
 keys C<Max> and C<Min>.
 
-  $r = new OMP::Range();
-  $r = new OMP::Range( Max => 5 );
+  $r = new Number::Interval();
+  $r = new Number::Interval( Max => 5 );
 
 =cut
 
@@ -77,12 +85,12 @@ sub new {
 
 =item B<max>
 
-Return (or set) the upper end of the range.
+Return (or set) the upper end of the interval.
 
   $max = $r->max;
   $r->max(22.0);
 
-C<undef> indicates that the range has no lower bound.
+C<undef> indicates that the interval has no upper bound.
 
 =cut
 
@@ -97,12 +105,12 @@ sub max {
 
 =item B<min>
 
-Return (or set) the lower end of the range.
+Return (or set) the lower end of the interval.
 
   $min = $r->min;
   $r->min( undef );
 
-C<undef> indicates that the range has no lower bound.
+C<undef> indicates that the interval has no lower bound.
 
 =cut
 
@@ -115,10 +123,10 @@ sub min {
 =item B<minmax>
 
 Return (or set) the minimum and maximum values of the
-range as an array.
+interval as an array.
 
   $r->minmax( 1, 5 );
-  @range = $r->minmax;
+  @interval = $r->minmax;
 
 Returns reference to an array in a scalar context.
 
@@ -137,10 +145,10 @@ sub minmax {
 =item B<minmax_hash>
 
 Return (or set) the minimum and maximum values of the
-range as an hash.
+interval as an hash.
 
   $r->minmax_hash( min => 1, max => 5 );
-  %range = $r->minmax_hash;
+  %interval = $r->minmax_hash;
 
 Returns reference to an hash in a scalar context.
 
@@ -205,9 +213,9 @@ sub stringify {
 
 =item B<isinverted>
 
-Determine whether the range is inverted. This is true if
+Determine whether the interval is inverted. This is true if
 both max and min are supplied but max is less than min. For all other
-cases (including unbound single-sided ranges) this will return false.
+cases (including unbound single-sided intervals) this will return false.
 
 =cut
 
@@ -224,8 +232,8 @@ sub isinverted {
 
 =item B<isbound>
 
-Returns true if the range is bound by an upper and lower limit.
-An inverted range would be bounded but inverted.
+Returns true if the interval is bound by an upper and lower limit.
+An inverted interval would be bounded but inverted.
 
 =cut
 
@@ -243,7 +251,7 @@ sub isbound {
 
 =item B<equate>
 
-Compare with another range object.
+Compare with another Interval object.
 Returns true if they are the same. False otherwise.
 
 =cut
@@ -254,7 +262,7 @@ sub equate {
 
   # Need to check that both are objects
   return 0 unless defined $comparison;
-  return 0 unless UNIVERSAL::isa($comparison, "OMP::Range");
+  return 0 unless UNIVERSAL::isa($comparison, "Number::Interval");
   no warnings 'numeric'; # KLUGE
   return 0 if $self->min != $comparison->min;
   return 0 if $self->max != $comparison->max;
@@ -263,9 +271,9 @@ sub equate {
 
 =item B<contains>
 
-Determine whether a supplied value is within the defined range.
+Determine whether a supplied value is within the defined intervals.
 
-  $is = $range->contains( $value );
+  $is = $i->contains( $value );
 
 =cut
 
@@ -276,20 +284,20 @@ sub contains {
   my $max = $self->max;
   my $min = $self->min;
 
-  # Assume it doesnt match the range
+  # Assume it doesnt match the interval
   my $contains = 0;
   if ($self->isinverted) {
-    # Inverted range. Both max and min must be defined
+    # Inverted interval. Both max and min must be defined
     if (defined $max and defined $min) {
       if ($value < $max || $value > $min) {
 	$contains = 1;
       }
     } else {
-      croak "A range can not be inverted with only one defined value";
+      croak "An interval can not be inverted with only one defined value";
     }
 
   } else {
-    # normal range
+    # normal interval
     if (defined $max and defined $min) {
       if ($value < $max && $value > $min) {
 	$contains = 1;
@@ -308,30 +316,30 @@ sub contains {
 
 =item B<intersection>
 
-Given another range object, modify the existing range to include
+Given another Interval object, modify the existing interval to include
 the additional constraints. For example, if the current object
-has a range of -3 to 10, and it is merged with an external object
-that has a range of 0 to 20 then the range of the current object
+has a interval of -3 to 10, and it is merged with an external object
+that has a interval of 0 to 20 then the interval of the current object
 will be converted to 0 to 10 since that is consistent with both
-ranges.
+intervals.
 
-  $status = $range->merge( $newrange );
+  $status = $interval->intersection( $newinterval );
 
-Returns true if the merge was successful.  If the ranges are
+Returns true if the intersection was successful.  If the intervals are
 incompatible (no intersection) or if no object was supplied returns
 false and the object is not modified.
 
-Intersections of an inverted range with a non-inverted range
+Intersections of an inverted interval with a non-inverted interval
 can, in some circumstances, result in an intersection covering
-two distinct bound ranges. This class can not yet support multiple
-ranges (that would make the intersection method even more of a nightmare)
+two distinct bound intervals. This class can not yet support multiple
+intervals (that would make the intersection method even more of a nightmare)
 so the routine dies if such a situation arises.
 
 =cut
 
 # There must be a neater way of implementing this method!
 # There may be some edge cases that fail (when one of the
-# range boundaries is identical in both objects)
+# interval boundaries is identical in both objects)
 
 sub intersection {
   my $self = shift;
@@ -339,7 +347,7 @@ sub intersection {
 
   # Check input
   return 0 unless defined $new;
-  return 0 unless UNIVERSAL::isa($new,"OMP::Range");
+  return 0 unless UNIVERSAL::isa($new,"Number::Interval");
 
   # Get the values
   my $max1 = $self->max;
@@ -358,8 +366,8 @@ sub intersection {
   my $outmax;
   my $outmin;
 
-  # There are six possible combinations of Bound range,
-  # inverted range and unbound range.
+  # There are six possible combinations of Bound interval,
+  # inverted interval and unbound interval.
 
   if ($bound) {
     # Support BB, BU and BI and II
@@ -391,20 +399,20 @@ sub intersection {
 	if ($nowbound) {
 	  # IB
 	  # We know that max2 and min2 are defined
-	  # We always end up with at least one bound range
+	  # We always end up with at least one bound interval
 	  if ($min2 < $max1) {
 	    $outmin = $min2;
 
-	    # If max2 is too high we get two ranges.
-	    croak "This intersection results in two output ranges. Currently not supported" if $max2 > $min1;
+	    # If max2 is too high we get two intervals.
+	    croak "This intersection results in two output intervals. Currently not supported" if $max2 > $min1;
 
-	    # Upper limit of range must be the min of the two maxes
+	    # Upper limit of interval must be the min of the two maxes
 	    $outmax = ( $max1 < $max2 ? $max1 : $max2 );
 
 	  } elsif ($min2 < $min1) {
 
 	    # Make sure we intersect a little
-	    # If the bound range lies outside the inverted range
+	    # If the bound interval lies outside the inverted interval
 	    # return undef
 	    if ($max2 >= $min1) {
 	      $outmin = $min1;
@@ -413,13 +421,13 @@ sub intersection {
 
 	  } elsif ($min2 > $min1) {
 
-	    # This is just the bound range
+	    # This is just the bound interval
 	    $outmin = $min2;
 	    $outmax = $max2;
 
 
 	  } else {
-	    croak "Oops Bug in range intersection [6]"
+	    croak "Oops Bug in interval intersection [6]"
 	  }
 
 
@@ -428,11 +436,11 @@ sub intersection {
 	  if (defined $max2) {
 
 	    # The upper bound must be below the inverted "min"
-	    # else we get intersection of two ranges
+	    # else we get intersection of two intervals
 	    if ($max2 > $min1) {
-	      croak "This intersection results in two output ranges. Currently not supported";
+	      croak "This intersection results in two output intervals. Currently not supported";
 	    } elsif ($max2 > $max1) {
-	      # Just use the inverted range
+	      # Just use the inverted interval
 	      $outmax = $max1;
 	      $outmin = $min1;
 	    } else {
@@ -445,11 +453,11 @@ sub intersection {
 	  } elsif (defined $min2) {
 
 	    # The lower bound must be above the "max"
-	    # else we get an intersection of two ranges
+	    # else we get an intersection of two intervals
 	    if ($min2 < $max1) {
-	      croak "This intersection results in two output ranges. Currently not supported";
+	      croak "This intersection results in two output intervals. Currently not supported";
 	    } elsif ($min2 < $min1) {
-	      # Just use the inverted range
+	      # Just use the inverted interval
 	      $outmax = $max1;
 	      $outmin = $min1;
 	    } else {
@@ -459,7 +467,7 @@ sub intersection {
 	    }
 
 	  } else {
-	    croak "This cant happen in OMP::Range[4]";
+	    croak "This cant happen in Number::Interval[4]";
 	  }
 
 	}
@@ -479,7 +487,7 @@ sub intersection {
 
 	# Check that we really are overlapping
 	if ($outmax < $outmin) {
-	  # oops - ranges did not intersect. Reset
+	  # oops - intervals did not intersect. Reset
 	  $outmin = $outmax = undef;
 	}
 	
@@ -493,39 +501,39 @@ sub intersection {
 	}
 
 	# unbound is now guaranteed to be (2)
-	# Check that unbound max is in range
+	# Check that unbound max is in interval
 	if (defined $max2) {
 	  if ($max2 < $max1 && $max2 > $min1) {
-	    # inside range
+	    # inside interval
 	    $outmax = $max2;
 	    $outmin = $min1;
 	  } elsif ($max2 < $min1) {
-	    # outside range. No intersection
+	    # outside interval. No intersection
 	  } elsif ($max2 > $max1) {
-	    # below range. irrelevant
+	    # below interval. irrelevant
 	    $outmax = $max1;
 	    $outmin = $min1;
 	  } else {
-	    croak "OMP::Range - This should not happen[2]";
+	    croak "Number::Interval - This should not happen[2]";
 	  }
 
 	} elsif (defined $min2) {
 	  if ($min2 < $max1 && $min2 > $min1) {
-	    # inside range
+	    # inside interval
 	    $outmax = $max1;
 	    $outmin = $min2;
 	  } elsif ($min2 > $max1) {
-	    # outside range. No intersection
+	    # outside interval. No intersection
 	  } elsif ($min2 < $min1) {
-	    # below range. irrelevant
+	    # below interval. irrelevant
 	    $outmax = $max1;
 	    $outmin = $min1;
 	  } else {
-	    croak "OMP::Range - This should not happen[3]";
+	    croak "Number::Interval - This should not happen[3]";
 	  }
 
 	} else {
-	  croak "range intersection: This cant happen (no limit defined)[1]";
+	  croak "interval intersection: This cant happen (no limit defined)[1]";
 	}
 
 
@@ -539,7 +547,7 @@ sub intersection {
     # Three options here. 
     # 1. A max and a max =>  max (same for min and min)
     # 2. max and a min with no overlap => no intersection
-    # 3. max and min with overlap => bounded range
+    # 3. max and min with overlap => bounded interval
     if (defined $max1 and defined $max2) {
       $outmax = ( $max1 > $max2 ? $max1 : $max2 );
     } else {
@@ -548,11 +556,11 @@ sub intersection {
       my $refmin = (defined $min1 ? $min1 : $min2);
 
       if ($refmax > $refmin) {
-	# normal bound range
+	# normal bound interval
 	$outmax = $refmax;
 	$outmin = $refmin;
       } else {
-	# unbound range. No intersection
+	# unbound interval. No intersection
       }
 
 
@@ -572,20 +580,29 @@ sub intersection {
 
 }
 
-
-
-
-
 =back
 
 =head1 COPYRIGHT
 
-Copyright (C) 2002 Particle Physics and Astronomy Research Council.
+Copyright (C) 2002-2004 Particle Physics and Astronomy Research Council.
 All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful,but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place,Suite 330, Boston, MA  02111-1307, USA
 
 =head1 AUTHOR
 
-Tim Jenness E<lt>t.jenness@jach.hawaii.eduE<gt>.
+Tim Jenness E<lt>tjenness@cpan.orgE<gt>.
 
 =cut
 
